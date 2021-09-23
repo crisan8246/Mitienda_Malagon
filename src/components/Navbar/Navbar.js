4f324f5c8f0b9af2a8de316850cb8d7b2885e058
crisan8246 <malgon8246.cm@gmail.com>
2021-09-22 21:53:27 -0500

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { MenuItems } from "./Menuitems"
+import './Navbar.css'
+
+class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked})
+    }
+
+
+
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+
+                </div>
+                <ul className={this.state.clicked ? 'na-menu active' : 'nav-menu'}>
+                    {MenuItems.map((items, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={items.cName} href={items.url}>
+                                    {items.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
+    }
+
+}
+
+export default Navbar
